f78edc79f2d45daa851349f0ddf7f25dc62b5923
CFCode-git <435184766@qq.com>
2020-04-02 18:49:00 +0800

    deploy

---
+++ precache-manifest.0837b9cb8d03e73c609890955b648d14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b5eeaa20021768294e",
+    "url": "/moneygrubber-1-website/css/app.d8e25f6b.css"
+  },
+  {
+    "revision": "eefdbde6ea616d3dd5c1c689e0ea8d7b",
+    "url": "/moneygrubber-1-website/index.html"
+  },
+  {
+    "revision": "72b5eeaa20021768294e",
+    "url": "/moneygrubber-1-website/js/app.493f21e5.js"
+  },
+  {
+    "revision": "43b0e175d158ec888ed2",
+    "url": "/moneygrubber-1-website/js/chunk-vendors.2ba44f86.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/moneygrubber-1-website/manifest.json"
+  },
+  {
+    "revision": "8f188c564c79b1f8d6b6be53b1b77f4b",
+    "url": "/moneygrubber-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneygrubber-1-website/robots.txt"
+  }
+]);
